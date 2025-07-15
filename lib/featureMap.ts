05cbdd2a6b7bfa786b990c6b448cc2708c7074c1
Zharfan <zharfanwidyadana@gmail.com>
2025-07-15 13:06:06 +0700

    membuat featureMap dalam file berbeda

---
+++ lib/featureMap.ts
@@ -0,0 +1,22 @@
+export const featureMap = {
+  "cnn-lstm": [
+    "sex", "generalhealth", "physicalhealthdays", "mentalhealthdays", "sleephours",
+    "hadasthma", "haddiabetes", "raceethnicitycategory", "agecategory", "bmi",
+    "physicalactivities", "removedteeth", "alcoholdrinkers", "fluvaxlast12",
+    "chestscan", "lastcheckuptime", "heightinmeters", "hadstroke", "hadcopd",
+    "hadarthritis", "hadkidneydisease", "hadskincancer", "haddepressivedisorder",
+    "deaforhardofhearing", "blindorvisiondifficulty", "difficultyconcentrating",
+    "difficultywalking", "difficultydressingbathing", "difficultyerrands",
+    "smokerstatus", "ecigaretteusage", "hivtesting", "pneumovaxever",
+    "tetanuslast10tdap", "highrisklastyear", "covidpos"
+  ],
+  "random-forest": [
+    "sex", "generalhealth", "physicalhealthdays", "mentalhealthdays", "sleephours",
+    "hadasthma", "haddiabetes", "raceethnicitycategory", "agecategory", "bmi"
+  ],
+  "mi": [
+    "physicalactivities", "hadasthma", "removedteeth", "alcoholdrinkers",
+    "fluvaxlast12", "chestscan", "sex", "generalhealth",
+    "raceethnicitycategory", "lastcheckuptime"
+  ]
+};
