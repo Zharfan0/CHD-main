7785a29cdedaa8e1a0d2b1267b68867784e5f5cf
Zharfan <zharfanwidyadana@gmail.com>
2026-03-03 13:08:27 +0700

    perbaikan url backend (deploy ulang)

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://chd-main.vercel.app"  # ✅ tambahkan domain vercel kamu di sini
+        "https://chd-backend-production-b702.up.railway.app"  # ✅ tambahkan domain vercel kamu di sini
     ],
     allow_credentials=True,
     allow_methods=["*"],
